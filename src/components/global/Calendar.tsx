b0c3ccab6369318c6479188c5860911780a6eb63
bitbyte08 <me@bitworkspace.kr>
2025-08-01 01:13:30 +0900

    feat :: 크기 props화

--- src/components/global/Calendar.tsx
+++ src/components/global/Calendar.tsx
@@ -1,25 +1,34 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import useCalendar from "@/hooks/useCalendar.ts";
 import DayButton from '@/components/Calendar/DayButton.tsx'
 
-
-const Calendar: React.FC = () => {
+interface CalendarProps {
+    height?: number | string;
+    width?: number | string;
+}
+const Calendar: React.FC<CalendarProps> = ({height, width}) => {
     const {data, year, month, day, setDay, Up, Down} = useCalendar();
 
     const ContainerStyle: React.CSSProperties = {
+        display: "flex",
+        flexDirection: "column",
+        width: width || "100%",
+        height: height || "100%",
+    }
+    const HeaderStyle: React.CSSProperties = {
+        height: "5rem",
         width: "100%",
-        height: "100%",
     }
     const CalendarStyle: React.CSSProperties = {
+        flex: 1,
         display: "grid",
         gridTemplateColumns: "repeat(7, 1fr)",
         gridTemplateRows: "repeat(6, 1fr)",
-        height: "100%",
         width: "100%",
     }
     return (
-        <div style={ContainerStyle}>
-            <header>
+        <article style={ContainerStyle}>
+            <header style={HeaderStyle}>
                 <button onClick={Down}>{"<"}</button>
                 <button onClick={Up}>{">"}</button>
                 <p>{year}</p>
@@ -28,20 +37,20 @@ const Calendar: React.FC = () => {
             <section style={CalendarStyle}>
                 {data && data.map((cWeek, i) =>
                     cWeek.map((cDay, j) => {
-
                         return (
                             <DayButton
                                 key={`${i}-${j}`}
                                 value={cDay}
                                 isSelected={cDay === day}
                                 onClick={() => {
+                                    if (cDay === null) return;
                                     if (cDay !== day) setDay(cDay);
                                 }}
                             />
                         )
                     }))}
             </section>
-        </div>
+        </article>
     )
 }
 
